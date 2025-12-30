0df77f4e09909b62278e30b45cff693f17ed422c
Christian-Desk <christiansilva.dev@outlook.com>
2025-12-30 20:47:05 -0300

    now we fix bugs and code running

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,19 @@
-import './Navbar.css'
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useState, useEffect } from 'react';
+import './Navbar.css';
+
 
 export default function Navbar() {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+    }
+  }, [darkMode]);
+
   return (
     <nav className="navbar">
       <span className="logo">CS</span>
@@ -10,7 +23,11 @@ export default function Navbar() {
         <li>Projects</li>
         <li>About</li>
         <li>Contact</li>
+        <li onClick={() => setDarkMode(!darkMode)} style={{cursor:'pointer'}}>
+          {darkMode ? <FaMoon /> : <FaSun />}
+        </li>
       </ul>
     </nav>
   )
 }
+
