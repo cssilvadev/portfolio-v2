bcc9646d30be51b4db1f964381f7f2aaa622e236
Christian-Desk <christiansilva.dev@outlook.com>
2026-01-06 22:33:56 -0300

    new branch created to tests

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -2,6 +2,7 @@ import "./Projects.css";
 
 const projects = [
   {
+     slug: "arm-robot",
     title: "Arm Robot ",
     description:
       "Firmware and desktop interface for a Arm Robot.",
@@ -10,6 +11,7 @@ const projects = [
     stack: ["STM32", "C", "C#", "WPF", "PWM"],
   },
   {
+    slug: "g27-pedal-adapter",
     title: "G27 Pedal Adapter",
     description:
       "Custom USB HID Pedal Adapter.",
@@ -18,6 +20,7 @@ const projects = [
     stack: ["STM32", "C", "USB HID"],
   },
   {
+    slug: "can-telemetry-monitor",
     title: "CAN Telemetry Monitor",
     description:
       "Real-time CAN bus analyzer with desktop interface.",
@@ -26,6 +29,7 @@ const projects = [
     stack: ["CAN", "C#", ".NET"],
   },
   {
+    slug: "interactive-portfolio",
     title: "Interactive Portfolio",
     description:
       "Personal portfolio with interactive visuals.",
@@ -34,6 +38,7 @@ const projects = [
     stack: ["React", "TypeScript", "CSS"],
   },
   {
+    slug: "quadruped-robot",
     title: "Quadruped Robot",
     description:
       "Quadruped robot with ESP32 using MAUI to control via Android Phone.",
@@ -84,7 +89,12 @@ export default function Projects() {
                     ))}
                   </ul>
 
-                  <button className="project-view">View</button>
+<a href="./projects/arm-robot.html" className="project-view">
+  View
+</a>
+
+
+
                 </div>
               </div>
 
